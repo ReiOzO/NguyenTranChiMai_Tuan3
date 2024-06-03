984ce73933c912f1d9914b720d0583aeb995abb2
ReiOzO <Zin21022003@gmail.com>
2024-06-03 22:32:09 +0700

    Lap trinh ung dung Java

---
+++ src/main/java/Tuan3/NguyenTranChiMai/NguyenTranChiMaiApplication.java
@@ -0,0 +1,13 @@
+package Tuan3.NguyenTranChiMai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  NguyenTranChiMaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NguyenTranChiMaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tuan3/NguyenTranChiMai/NguyenTranChiMaiApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan3.NguyenTranChiMai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NguyenTranChiMaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
